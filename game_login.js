7ba4f76c6f041848c22dce9ed3aa4726b3a2648e
Emiliano9G <138831959+Emiliano9G@users.noreply.github.com>
2023-10-31 17:39:06 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+function addUser()
+{
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+
+//clase 91
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location="gammaboy.html";
+}
